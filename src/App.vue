f055094a26f8b46478b6883808be73c0618dedb9
leandro_matos <59984034+leandro-matos@users.noreply.github.com>
2020-04-27 19:12:39 -0300

    Adicionando o front-end do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,142 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <nav>
+      <div class="nav-wrapper blue darken-1">
+        <a href="#" class="brand-logo center">LISTA DE PRODUTOS</a>
+      </div>
+    </nav>
+
+    <div class="container">
+
+
+
+      <form @submit.prevent="salvar">
+
+          <label>Nome</label>
+          <input type="text" id="nome" placeholder="Nome" v-model="produto.nome" required name=nome>
+          <label>Quantidade</label>
+          <input type="number" id="qtde" placeholder="QTD" v-model="produto.quantidade" required name=qtde>
+          <label>Valor</label>
+          <input type="text" id="valor" placeholder="Valor" v-model="produto.valor" required name=valor>
+
+          <button class="waves-effect waves-light btn-small">Salvar<i class="material-icons left">save</i></button>
+
+      </form>
+
+      <table>
+
+        <thead>
+
+          <tr>
+            <th>NOME</th>
+            <th>QUANTIDADE</th>
+            <th>VALOR</th>
+            <th>OPÇÕES</th>
+          </tr>
+
+        </thead>
+
+        <tbody>
+
+          <tr v-for="produto of produtos" :key="produto.id">
+
+            <td>{{ produto.nome }}</td>
+            <td>{{ produto.quantidade }}</td>
+            <td>{{ produto.valor }}</td>
+            <td>
+              <button @click="editar(produto)" class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
+              <button @click="remover(produto)" class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
+            </td>
+
+          </tr>
+
+        </tbody>
+      
+      </table>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Produto from './services/produtos'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+    data(){
+      return {
+        produto: {
+          id: '',
+          nome: '',
+          quantidade: '',
+          valor: ''
+
+        },
+        produtos: [],
+        errors: []
+      }
+    },
+
+  mounted(){ 
+      this.listar()
+
+  },
+
+  methods:{
+
+    listar(){
+      Produto.listar().then(resposta => {
+        this.produtos = resposta.data
+      })
+    },
+
+    salvar(){
+      if(!this.produto.id){
+        Produto.salvar(this.produto).then(resposta => {
+          this.produto = {}
+          alert('Salvo com Sucesso!')
+          this.resposta = resposta
+          this.listar();
+          this.errors = []
+        }).catch(e => {
+          this.errors = e.response.data.errors
+        })
+
+      }else{
+        Produto.atualizar(this.produto).then(resposta => {
+          this.produto = {}
+          alert('Atualizado com Sucesso!')
+          this.resposta = resposta
+          this.listar();
+          this.errors = []
+        }).catch(e => {
+          this.errors = e.response.data.errors
+        })
+    }
+
+    },
+
+    editar(produto){
+      this.produto = produto
+    },
+
+    remover(produto){
+      if(confirm('Deseja excluir o produto?')){
+        Produto.apagar(produto).then(resposta => {
+          this.resposta = resposta
+          this.listar()
+          this.errors = {}
+        }).catch(e => {
+          this.errors = e.response.data.errors
+        })
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/services/produtos.js
@@ -0,0 +1,21 @@
+import { http } from  './config'
+
+export default {
+
+    listar:() => {
+        return http.get('produtos')
+    },
+
+    salvar:(produto) => {
+        return http.post('produto', produto);
+    },
+
+    atualizar:(produto) => {
+        return http.put('produto', produto);
+    },
+
+    apagar:(produto)=>{
+		return http.delete('produto', { data: produto })
+	}
+
+}
